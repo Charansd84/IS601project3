16571e3abe5a5bbdbb5c7793a37544f5b008190f
kiran <charandegala1998@gmail.com>
2022-04-20 16:50:06 -0400

    commit 10.

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -1,109 +1,66 @@
+"""A simple flask web app"""
+import flask_login
+import os
+import datetime
+import time
+
+from flask import g, request
+from rfc3339 import rfc3339
+
+from flask import render_template, Flask, has_request_context, request
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+
+from app.auth import auth
+from app.auth.user_operation import user_operations
+from app.auth import auth
+from app.cli import create_database, create_log_folder
+from app.context_processors import utility_text_processors
+from app.logging_config import log_con
+from app.db import db
+from app.db.models import User
+from app.exceptions import http_exceptions
+from app.simple_pages import simple_pages
 import logging
-from logging.config import dictConfig
+from flask.logging import default_handler
 
-import flask
-from flask import request, current_app, blueprints
-
-from app.logging_config.log_formatters import RequestFormatter
-
-log_con = flask.Blueprint('log_con', __name__)
+login_manager = flask_login.LoginManager()
 
 
-@log_con.before_app_request
-def before_request_logging():
-    current_app.logger.info("Before Request")
-    log = logging.getLogger("myApp")
-    log.info("My App Logger")
-    log = logging.getLogger("myDebug")
-    log.debug("My Debug Logger Message")
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
-@log_con.after_app_request
-def after_request_logging(response):
-    if request.path == '/favicon.ico':
-        return response
-    elif request.path.startswith('/static'):
-        return response
-    elif request.path.startswith('/bootstrap'):
-        return response
-    current_app.logger.info("After Request")
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap5(app)
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+    app.register_blueprint(log_con)
+    app.register_blueprint(user_operations)
+    app.context_processor(utility_text_processors)
+    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
+    app.register_error_handler(404, page_not_found)
+    # app.add_url_rule("/", endpoint="index")
+    db_dir = "database/db.sqlite"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    # add command function to cli commands
+    app.cli.add_command(create_database)
+    app.cli.add_command(create_log_folder)
 
-    log = logging.getLogger("myApp")
-    log.info("My App Logger")
-    log = logging.getLogger("myDebug")
-    log.debug("My Debug Logger Message")
-    return response
+    return app
 
 
-@log_con.before_app_first_request
-def configure_logging():
-    logging.config.dictConfig(LOGGING_CONFIG)
-    log = logging.getLogger("myApp")
-    log.info("My App Logger")
-    log = logging.getLogger("myDebug")
-    log.debug("My Debug Logger Message")
-
-
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-        'RequestFormatter': {
-            '()': 'app.logging_config.log_formatters.RequestFormatter',
-            'format': '[%(asctime)s] [%(process)d] %(remote_addr)s requested %(url)s'
-                      '%(levelname)s in %(module)s: %(message)s'
-        },
-        'DebugFormatter': {
-            '()': 'app.logging_config.log_formatters.RequestFormatter',
-            'format': '%(levelname)s : %(message)s'
-        }
-    },
-    'handlers': {
-        'default': {
-            'level': 'DEBUG',
-            'formatter': 'standard',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',  # Default is stderr
-        },
-        'file.handler.myapp': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'RequestFormatter',
-            'filename': 'app/logs/myinfo.log',
-            'maxBytes': 10000000,
-            'backupCount': 5,
-        },
-        'file.handler.mydebug': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'DebugFormatter',
-            'filename': 'app/logs/mydebug.log',
-            'maxBytes': 10000000,
-            'backupCount': 5,
-        },
-    },
-    'loggers': {
-        '': {  # root logger
-            'handlers': ['default'],
-            'level': 'DEBUG',
-            'propagate': True
-        },
-        '__main__': {  # if __name__ == '__main__'
-            'handlers': ['default'],
-            'level': 'DEBUG',
-            'propagate': True
-        },
-        'myApp': {  # if __name__ == '__main__'
-            'handlers': ['file.handler.myapp'],
-            'level': 'INFO',
-            'propagate': False
-        },
-        'myDebug': {  # if __name__ == '__main__'
-            'handlers': ['file.handler.mydebug'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-
-    }
-}
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
