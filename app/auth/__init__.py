5fdd91205e94e2cdb03fc9fd927fc39cdeadc372
kiran <charandegala1998@gmail.com>
2022-04-30 21:41:47 -0400

    New commit 18.

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -10,7 +10,7 @@ from app.db.models import User
 auth = Blueprint('auth', __name__, template_folder='templates')
 
 
-@auth.route('register', methods=['POST', 'GET'])
+@auth.route('/register', methods=['POST', 'GET'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('auth.dashboard'))
@@ -169,4 +169,7 @@ def delete_user(user_id):
     db.session.commit()
     flash('User Deleted', 'success')
     return redirect(url_for('auth.browse_users'), 302)
+
+
+
 
